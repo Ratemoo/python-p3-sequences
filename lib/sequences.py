7a9e2a4e747a1957c47ed6d62a2ee1a48254334c
Brian Ratemo Magucha <brianratemom@gmail.com>
2024-08-28 09:37:20 +0300

    ..

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+        print('[]')
+        return
+    
+    fibonacci_sequence = []
+    a, b = 0, 1
+    while length > 0:
+        fibonacci_sequence.append(a)
+        a, b = b, a + b
+        length -= 1
+    
+    print(fibonacci_sequence)
